0ba2b6bc269f6586e9d93d0f4283d375d341d177
Coba <coba@cobac.eu>
2023-05-30 18:54:39 +0200

    initial state

--- keyboards/crkbd/keymaps/cobac/keymap.c
+++ keyboards/crkbd/keymaps/cobac/keymap.c
@@ -18,59 +18,150 @@
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+#include <keymap_spanish.h>
+
+// Magic for alt_t(/) starts
+// From: https://docs.qmk.fm/#/feature_tap_dance?id=example-5-using-tap-dance-for-advanced-mod-tap-and-layer-tap-keys
+enum td_keycodes {
+		ALT_S7
+};
+
+typedef enum {
+		TD_NONE,
+		TD_UNKNOWN,
+		TD_SINGLE_TAP,
+		TD_SINGLE_HOLD,
+		TD_DOUBLE_SINGLE_TAP
+} td_state_t;
+
+static td_state_t td_state;
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+void alts7_finished(qk_tap_dance_state_t *state, void *user_data);
+void alts7_reset(qk_tap_dance_state_t *state, void *user_data);
+// Magic for alt_t(/) ends
+// More after layout
 
 // List of keycodes: https://docs.qmk.fm/#/keycodes
+
+// TODO: Workspaces on top row lol
+// TODO: left-right arrow somewhere that can be holded
+// TODO: Make sure that I can do mod + enter
+// TODO: Make sure that I can do mod + enter
+// TODO: VOLUME
+// TODO: Brightness and backlight control
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x6_3(
 	//,-----------------------------------------------------.										 ,-----------------------------------------------------.
-			 KC_TAB,		KC_Q,		 KC_W,		KC_E,		 KC_R,		KC_T,													KC_Y,		 KC_U,		KC_I,		 KC_O,	 KC_P,	KC_BSPC,
+			 XXXXXXX,		KC_Q,		 KC_W,		KC_E,		 KC_R,		KC_T,													KC_Y,		 KC_U,		KC_I,		 KC_O,	 KC_P,	KC_BSPC,
+	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------| TODO: +
+	 LT(0, KC_ESC),	KC_A,		 KC_S,		KC_D,		 KC_F,		KC_G,													KC_H,		 KC_J,		KC_K,		 KC_L, KC_SCLN, KC_QUOT,
 	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LCTL,		KC_A,		 KC_S,		KC_D,		 KC_F,		KC_G,													KC_H,		 KC_J,		KC_K,		 KC_L, KC_SCLN, KC_QUOT,
-	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LSFT,		KC_Z,		 KC_X,		KC_C,		 KC_V,		KC_B,													KC_N,		 KC_M, KC_COMM,	 KC_DOT, KC_SLSH,	 KC_ESC,
+			XXXXXXX,		KC_Z,		 KC_X,		KC_C,		 KC_V,		KC_B,													KC_N,		 KC_M, KC_COMM,	 KC_DOT, KC_SLSH,	 XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
-																					KC_LGUI,	 MO(1),	 KC_SPC,		 KC_ENT,	SFT_T(KC_SPC) , KC_RALT
-																			//`--------------------------'	`--------------------------'
+										TD(ALT_S7),	LCTL_T(KC_TAB),	LGUI_T(XXXXXXX),						 LT(1, KC_ENTER),	SFT_T(KC_SPC) , LT(2, XXXXXXX)
+			 //`--------------------------'	`--------------------------'
 
-	),
+                           ),
 
-	[1] = LAYOUT_split_3x6_3(
-	//,-----------------------------------------------------.										 ,-----------------------------------------------------.
-			 KC_TAB,		KC_1,		 KC_2,		KC_3,		 KC_4,		KC_5,													KC_6,		 KC_7,		KC_8,		 KC_9,		KC_0, KC_BSPC,
-	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 KC_LEFT, KC_DOWN,	 KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
-	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+	[1] = LAYOUT_split_3x6_3( // Symbols
+	//,---------------------------------------------------------------|						 |------------------------------------------------------------------------------------.
+		KC_BSLS,		XXXXXXX,		S(KC_5),		S(KC_EQL), S(KC_MINS), RALT(KC_BSLS),			RALT(KC_3),	RALT(KC_LBRACKET), RALT(KC_RBRACKET),	KC_MINS,			S(KC_LBRC), S(KC_RBRC),
+	//|--------+--------+--------+--------+--------+-------------------------|		 |--------+--------+--------+--------+--------+------------------------------------|
+		RALT(KC_E), XXXXXXX,		S(KC_6),		KC_EQL,		 S(KC_1),		 RALT(KC_QUOT),			S(KC_2),		S(KC_8),					 S(KC_9),						RALT(KC_GRV), S(KC_0),		KC_LBRC, 
+	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------------------------------------|
+		RALT(KC_6), RALT(KC_4), RALT(KC_1), KC_NUBS,	 S(KC_NUBS), RALT(KC_2),				S(KC_4),		S(KC_SLSH),				 S(KC_COMM),				S(KC_DOT),		S(KC_3),		RESET,
 	//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
-																					KC_LGUI, _______,	 KC_SPC,		 KC_ENT,	 MO(3), KC_RALT
-																			//`--------------------------'	`--------------------------'
-	),
-
-	[2] = LAYOUT_split_3x6_3(
-	//,-----------------------------------------------------.										 ,-----------------------------------------------------.
-			 KC_TAB, KC_EXLM,		KC_AT, KC_HASH,	 KC_DLR, KC_PERC,											 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 KC_MINS,	 KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,	 KC_GRV,
-	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-	//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
-																					KC_LGUI,	 MO(3),	 KC_SPC,		 KC_ENT, _______, KC_RALT
+																						S(KC_MINS), _______,	 _______,							 _______,	_______ , _______
 																			//`--------------------------'	`--------------------------'
 	),
 
-	[3] = LAYOUT_split_3x6_3(
+	[2] = LAYOUT_split_3x6_3( // Numbers
 	//,-----------------------------------------------------.										 ,-----------------------------------------------------.
-				RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+			XXXXXXX, XXXXXXX,	XXXXXXX, XXXXXXX,	 XXXXXXX, XXXXXXX,											XXXXXXX,	KC_7,		 KC_8,		KC_9,	 XXXXXXX, XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+			 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 KC_0,		KC_4,		 KC_5,		KC_6,	 XXXXXXX, XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
-			RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+			XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 XXXXXXX,		KC_1,		 KC_2,		KC_3,	 XXXXXXX, XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
-																					KC_LGUI, _______,	 KC_SPC,		 KC_ENT, _______, KC_RALT
+																				_______,	 _______,	 _______,		 _______, _______, _______
 																			//`--------------------------'	`--------------------------'
+	),
+
+	[3] = LAYOUT_split_3x6_3( // Movement
+		//,-----------------------------------------------------.										 ,-----------------------------------------------------.
+				XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,	 XXXXXXX, XXXXXXX,										 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
+				 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,										 KC_LEFT, KC_DOWN, KC_UP,		KC_RIGHT, XXXXXXX, XXXXXXX,
+		//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
+				XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
+																			_______,	 _______,	 _______,		 _______, _______, _______
+																		//`--------------------------'	`--------------------------'
 	)
 };
+
+// Empty layer template
+//		//,-----------------------------------------------------.										 ,-----------------------------------------------------.
+//				XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,	 XXXXXXX, XXXXXXX,										 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//		//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
+//				 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,										 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//		//|--------+--------+--------+--------+--------+--------|										 |--------+--------+--------+--------+--------+--------|
+//				XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,											 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//		//|--------+--------+--------+--------+--------+--------+--------|	|--------+--------+--------+--------+--------+--------+--------|
+//																			_______,	 _______,	 _______,		 _______, _______, _______,
+																		//`--------------------------'	`--------------------------'
+
+// Magic for alt_t(/) starts
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+		if (state->count == 1) {
+				if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+				else return TD_SINGLE_HOLD;
+		}
+
+		if (state->count == 2) return TD_DOUBLE_SINGLE_TAP;
+		else return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+void alts7_finished(qk_tap_dance_state_t *state, void *user_data) {
+		td_state = cur_dance(state);
+		switch (td_state) {
+				case TD_SINGLE_TAP:
+					register_code16(S(KC_7));
+						break;
+				case TD_SINGLE_HOLD:
+						register_mods(MOD_BIT(KC_LALT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+						break;
+				case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
+						tap_code16(S(KC_7));
+						register_code16(S(KC_7));
+    default:
+      break;
+    }
+}
+
+void alts7_reset(qk_tap_dance_state_t *state, void *user_data) {
+		switch (td_state) {
+				case TD_SINGLE_TAP:
+						unregister_code16(S(KC_7));
+						break;
+				case TD_SINGLE_HOLD:
+						unregister_mods(MOD_BIT(KC_LALT)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
+						break;
+				case TD_DOUBLE_SINGLE_TAP:
+						unregister_code16(S(KC_7));
+     default:
+       break;
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+		[ALT_S7] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alts7_finished, alts7_reset)
+};
+
+// Magic for alt_t(/) ends
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
